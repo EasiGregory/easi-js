522480ab009811901d7fc219ce395787655e4cda
s0043955 <gregory.mcwilliams@rgare.com>
2017-03-10 15:57:13 -0600

    Problem 1-4 Changing to a named function instead of declaring an anonymous function and assigning it to a variable for readability

--- app/euler/multiples.js
+++ app/euler/multiples.js
@@ -1,4 +1,4 @@
-let divisibleNumbers = function () {
+function divisibleNumbers() {
     let total = 0;
     for (let i = 0; i < 1000; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
@@ -6,6 +6,6 @@ let divisibleNumbers = function () {
         }
     }
     return total;
-};
+}
 
 console.log('Total of all the numbers that are divisible by 3 or 5 are: ' + divisibleNumbers());
